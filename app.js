47f5b26c2c612b4d6c0773eae71d281a37954a5c
Aditya Ajay <adityaajay@Adityas-MacBook-Air.local>
2022-06-13 02:00:22 +0530

    test port again

--- app.js
+++ app.js
@@ -154,10 +154,6 @@ app.post("/compose", function(req, res)
 
 // chosing port process.env.PORT || 3000 to run on www ----- 6
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 app.listen(3000, function () {
   console.log("server started successfully");
 });
