4d48aba5e9852b55f1d35133a6617ac8513b27ee
Aditya Ajay <adityaajay@Adityas-MacBook-Air.local>
2022-06-13 01:56:06 +0530

    test port error

--- app.js
+++ app.js
@@ -158,6 +158,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(process.env.PORT || port, function () {
+app.listen(3000, function () {
   console.log("server started successfully");
 });
